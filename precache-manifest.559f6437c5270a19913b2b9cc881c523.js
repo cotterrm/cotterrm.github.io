81ff7cd1e856117808f974dbe1c9b7e0a8d442ca
Ricky C <rick.cotter@gmail.com>
2024-05-02 17:45:01 -0500

    Updates

---
+++ precache-manifest.559f6437c5270a19913b2b9cc881c523.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fd7c1b9efd08833e716",
+    "url": "/me/static/css/main.2652ca8a.chunk.css"
+  },
+  {
+    "revision": "0fd7c1b9efd08833e716",
+    "url": "/me/static/js/main.0fd7c1b9.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "f79e20e961669b39e52106ccfbaec2e0",
+    "url": "/me/index.html"
+  }
+];
